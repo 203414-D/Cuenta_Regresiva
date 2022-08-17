9898efc35bbc90e0c87ded26f8f657eda0fb080d
203437-A <203437@ids.upchiapas.edu.mx>
2022-08-16 20:02:03 -0500

    Se agrego un icono y un menu de la cuenta regresiva

--- temporizador/plugin-temporizador.php
+++ temporizador/plugin-temporizador.php
@@ -3,9 +3,25 @@
 // Plugin name: Temporizador
 // Description: Plugin para temporizador
 // Version: 1.0
-// Author: Sebastián ramirez
+// Author: Sebastián ramirez, Diego Chacon, Ángel Gómez, Gerson López.
 // Author URI: https://github.com/203419
 
+add_action('admin_menu','CrearMenu');
 
+function CrearMenu(){
+    add_menu_page(
+        'Cuenta regresiva',
+        'Cuenta regresiva (timer)',
+        'manage_options',
+        'sp_menu',
+        'ContenidoDelPlugin',
+         plugin_dir_url(__FILE__).'icono/img/icono.svg', 
+         '1'
+    );
+}
+
+function ContenidoDelPlugin(){
+    echo "<h1>Ejemplo del contenido del plugin </h1>";
+}
 
 ?>
