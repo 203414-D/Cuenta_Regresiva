13d4e1a29392d85430c800bd24e8872b48612237
203718 <203718@ids.upchiapas.edu.mx>
2022-08-19 16:15:04 -0500

    realice cambios en el contador

--- temporizador/admin/Temporizador.php
+++ temporizador/admin/Temporizador.php
@@ -26,7 +26,7 @@
                 var distance = countDownDate - now;
                 var days = Math.floor(distance/(1000 * 60 * 60 *24));
                 var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
-                var minutes = Math.floor((distance%(1000*60*60*24))/(1000*60));
+                var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
                 document.getElementById("demo").innerHTML = "<div class="+"container"+">"+days + "D/" + " "+hours + "H/" + " "+minutes + "MIN"+"</div>";
                 if(distance <0){
                     clearInterval(x);
