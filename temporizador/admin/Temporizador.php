ea2e417d80a548f0b82ac9abbc599c9c578a9e01
203718 <203718@ids.upchiapas.edu.mx>
2022-08-19 16:04:21 -0500

    agregue estilos de css

--- temporizador/admin/Temporizador.php
+++ temporizador/admin/Temporizador.php
@@ -4,35 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=T">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../wp-content/plugins/temporizador/css/style.css">
     <title>Cuenta Regresiva</title>
 </head>
-<body>
-    <?php 
-    $date = date('2022-08-18');
-    $time = ('12:30:00');
-    $date_today = $date . ' '. $time;
-    echo '<p>correra hasta la fecha</p>';
-    echo '<P>'.$date_today;'</p>';
-     ?>
-    <script type ="text/javascript">
-        var count_id = "<?php echo $date_today;?>";
-        var countDownDate = new Date(count_id).getTime();
-        var x = setInterval(function(){
-            var now = new Date().getTime();
-            var distance = countDownDate - now;
-            var days = Math.floor(distance/(1000 * 60 * 60 *24));
-            var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
-            var minutes = Math.floor((distance%(1000*60*60*24))/(1000*60));
 
-            document.getElementById("demo").innerHTML = days + "d" + " "+hours + "h" + " "+minutes + "min";
-            if(distance <0){
-                clearInterval(x);
-                document.getElementById("demo").innerHTML="Expirado";
-            }
-        }, 1000);
-    </script>
-    <?php 
-        echo '<p id= "demo" ></p>';
-    ?>
+<body>
+    <div>
+        <?php
+        $date = date('2022-08-22');
+        $time = ('12:30:00');
+        $date_today = $date . ' '. $time;
+        echo '<h1>CORRERÁ HASTA LA FECHA</h1>';
+        echo '<P>'.$date_today;'</p>';
+        ?>
+        <script type ="text/javascript">
+            var count_id = "<?php echo $date_today;?>";
+            var countDownDate = new Date(count_id).getTime();
+            var x = setInterval(function(){
+                var now = new Date().getTime();
+                var distance = countDownDate - now;
+                var days = Math.floor(distance/(1000 * 60 * 60 *24));
+                var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+                var minutes = Math.floor((distance%(1000*60*60*24))/(1000*60));
+                document.getElementById("demo").innerHTML = "<div class="+"container"+">"+days + "D/" + " "+hours + "H/" + " "+minutes + "MIN"+"</div>";
+                if(distance <0){
+                    clearInterval(x);
+                    document.getElementById("demo").innerHTML="Expirado";
+                }
+            }, 1000);
+        </script>
+        <?php 
+            echo '<p id= "demo" ></p>';
+        ?>
+    </div>
+    
+
 </body>
 </html>
